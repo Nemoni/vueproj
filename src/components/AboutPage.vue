34ab8fdaf40e7ee6875f51c49b085984caaea5f6
nemo <490202694@qq.com>
2023-03-09 03:17:26 +0000

    vue-router动态路由，router-link由for循环生成

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -4,17 +4,28 @@
   <div>
     <h2>About Component</h2>
     <p>{{ message }}</p>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <router-link :to="`/about/${user.id}`">{{ user.name }}</router-link>
+      </li>
+    </ul>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: "AboutPage",
+  name: 'AboutPage',
   data() {
     return {
-      message: "This is the about page!",
-    };
-  },
-};
+      message: 'This is the about page!',
+      users: [
+        { id: 1, name: 'Alice' },
+        { id: 2, name: 'Bob' },
+        { id: 3, name: 'Charlie' }
+      ]
+    }
+  }
+}
+
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../components/HomePage.vue'
 import About from '../components/AboutPage.vue'
-import AboutIndex from '../components/AboutIndex.vue'
 import AboutDetails from '../components/AboutDetails.vue'
 
 const routes = [
@@ -18,29 +17,23 @@ const routes = [
     component: About,
     children: [
       {
-        path: '',
-        name: 'AboutIndex',
-        component: AboutIndex
-      },
-      {
         path: ':id',
         name: 'AboutDetails',
         component: AboutDetails
       }
     ]
+  },
+  {
+    path: '/:catchAll(.*)',
+    redirect: '/'
   }
 ]
-
 /*
-在 Vue Router 4 中，我们可以使用嵌套路由来实现页面的嵌套和组合。嵌套路由指的是在一个路由下，还可以包含其他子路由。
-在上面的示例代码中，我们定义了一个名为 About 的路由，它包含两个子路由：AboutIndex 和 AboutDetails。
-当用户访问 /about 路径时，会默认渲染 AboutIndex 组件；当用户访问 /about/:id 路径时，会渲染 AboutDetails 组件，
-并将路由参数 id 传递给组件。
-在路由配置中，嵌套路由使用 children 属性来指定，它是一个数组，其中每个元素都是一个路由对象，可以嵌套多层。
-除了嵌套路由，Vue Router 4 还提供了命名路由的功能，它可以让我们为某个路由指定一个名称，并在程序中通过这个名称来进行导航。
-在程序中，我们可以使用 $router.push({ name: 'AboutIndex' }) 来导航到 AboutIndex 路由，而不必使用路径字符串来指定路由。
+在 Vue Router 4 中，我们可以使用动态路由来实现根据不同参数值动态匹配不同的路由。
+例如，在上面的示例代码中，我们为 /about/:id 这个路由规则定义了一个动态参数 id，它可以匹配任何以 /about/ 开头的路径，
+并将其后面的参数值传递给路由组件。在 AboutPage 组件中，我们使用 v-for 遍历了一个包含多个用户信息的数组，
+并使用 router-link 来动态生成路由链接。
 */
-
 const router = createRouter({
   history: createWebHashHistory(),
   routes
